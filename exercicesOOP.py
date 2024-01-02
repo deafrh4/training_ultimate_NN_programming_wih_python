74b7f5b3fb7aa046f38e908a776b16fb58b72d4b
Dorra EA <dorra.elabed2020@outlook.fr>
2024-01-02 19:32:00 +0100

    requirements added

--- exercicesOOP.py
+++ exercicesOOP.py
@@ -1,19 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
 
+# url of the website to scrap
+url = "http://quotes.toscrape.com"
 
-#url of the website to scrap
-url="http://quotes.toscrape.com"
+# send a get request to the website
+response = requests.get(url)
 
-#send a get request to the website
-response =requests.get(url)
+# parse the page content
+soup = BeautifulSoup(response.text,"html.parser")
 
-#parse the page content
-soup=BeautifulSoup(response.text,"html.parser")
+# extract quotes
+quotes = soup.find_all('span',class_='text')
 
-#extract quotes
-quotes =soup.find_all('span',class_='text')
-
-#print the first 5 quotes
+# print the first 5 quotes
 for quote in quotes[:5] :
     print(quote.get_text())
